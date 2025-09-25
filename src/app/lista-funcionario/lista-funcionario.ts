6fd0e6ec2173595a0c70f3db2c831703f3daf355
Fabio Luiz <devfabioluiz@gmail.com>
2025-09-24 21:39:25 -0300

    .

--- src/app/lista-funcionario/lista-funcionario.ts
+++ src/app/lista-funcionario/lista-funcionario.ts
@@ -2,28 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Funcionarios } from '../services/funcionarios';
 import { Router } from '@angular/router';
 
-import { MatTableModule } from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-lista-funcionario',
   standalone: false,
@@ -32,19 +10,19 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ListaFuncionario implements OnInit {
   constructor(private funcionarios: Funcionarios, private router: Router) {}
-
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  funcionariosLista: any[] = [];
+  displayedColumns: string[] = ['nome', 'sobrenome', 'dtNascimento', 'salario'];
+  dataSource = this.funcionariosLista;
 
   ngOnInit() {
     this.getFuncionarios();
   }
 
-  funcionariosLista: any[] = [];
-
   getFuncionarios() {
     this.funcionarios.getUsers().subscribe((data: any) => {
       this.funcionariosLista = data;
+      console.log(this.funcionariosLista);
+      
     });
   }
 
